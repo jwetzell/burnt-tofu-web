dea2ff7d4ceb2cda4f50404fb40997916fb5fe3f
Ethan Massie <ethan.massie1@gmail.com>
2020-04-04 20:53:46 -0500

    added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WanikaniApiNgModule } from 'wanikani-api-ng';
+import { WanikaniTokenModule } from 'wanikani-api-ng';
+import { LoginComponent } from './login/login.component'
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    WanikaniApiNgModule,
+    WanikaniTokenModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
