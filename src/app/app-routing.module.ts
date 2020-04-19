20d8c3f67389ac9cee22861ecc640a0ebd6a85c6
Ethan Massie <ethan.massie1@gmail.com>
2020-04-19 17:38:33 -0500

    added redirect to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TokenGuard } from './guards/token.guard';
+import { AuthGuard } from 'wanikani-api-ng';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: '', canActivate: [TokenGuard],loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+  { path: 'home', canActivate: [AuthGuard],loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
