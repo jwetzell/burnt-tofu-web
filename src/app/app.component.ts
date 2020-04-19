51a582b1d80c0591d42ebaa5b565f2a1af12c29c
Ethan Massie <ethan.massie1@gmail.com>
2020-04-19 17:39:04 -0500

    watched isAuthenticated and navigated to the home page if you are and login page if not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WanikaniTokenService } from 'wanikani-api-ng';
+import { filter } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'burnt-tofu-web';
+export class AppComponent implements OnInit {
+  title = 'Burnt Tofu';
+
+  constructor(private tokenService: WanikaniTokenService, private router: Router) {}
+
+  ngOnInit() {
+    this.tokenService.getIsAuthenticated().pipe().subscribe(
+      authenticated => {
+        if(authenticated)
+          this.router.navigate(['/home']);
+        else
+          this.router.navigate(['/login']);
+      }
+    )
+  }
+
 }
